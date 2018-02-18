d5657fa4ee55eea3f9df87664a343ca85eb8ef86
tjan <tjanfei.chao@gmail.com>
2018-02-18 22:30:28 +0100

    finished adapter for listView

---
+++ app/src/main/java/CountryListAdapter.java
@@ -0,0 +1,72 @@
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import be.ehb.listviewoef.R;
+
+/**
+ * Created by Q on 18-2-2018.
+ */
+
+public class CountryListAdapter extends BaseAdapter {
+
+    private ArrayList<Country>countryList;
+
+    private Activity activity;
+
+    private static class ViewHolder {
+        private TextView tvName;
+        private TextView tvLanguage;
+    }
+
+    public CountryListAdapter(ArrayList<Country> countryList, Activity activity) {
+        this.countryList = countryList;
+        this.activity = activity;
+    }
+
+    @Override
+    public int getCount() {
+        return countryList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return countryList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        ViewHolder row = new ViewHolder();
+
+
+        if (view == null) {
+            view = activity.getLayoutInflater().inflate(R.layout.list_item_countries,null);
+
+            row.tvName = view.findViewById(R.id.tv_name);
+            row.tvLanguage = view.findViewById(R.id.tv_language);
+
+            view.setTag(row);
+        }
+        else {
+            row = (ViewHolder) view.getTag();
+        }
+
+        Country currentCountry = countryList.get(i);
+
+        row.tvName.setText(currentCountry.getName());
+        row.tvName.setText(currentCountry.getLanguage());
+
+
+        return view;
+    }
+}
